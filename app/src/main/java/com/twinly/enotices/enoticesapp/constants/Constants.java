34641e9645dac1c4d8c6b7cc6146344fc280aa27
jbhuang@comp.hkbu.edu.hk <jbhuang@comp.hkbu.edu.hk>
2018-01-29 16:53:01 +0800

    1-29 huang jinbin

---
+++ app/src/main/java/com/twinly/enotices/enoticesapp/constants/Constants.java
@@ -0,0 +1,10 @@
+package com.twinly.enotices.enoticesapp.constants;
+
+/**
+ * Created by jbhuang on 1/26/2018.
+ */
+
+public class Constants {
+
+    public static String BASE_URL="https://credential.top";
+}
---
+++ app/src/main/java/com/twinly/enotices/enoticesapp/utils/network/OnResponseListener.java
@@ -0,0 +1,12 @@
+package com.twinly.enotices.enoticesapp.utils.network;
+
+/**
+ * Created by jbhuang on 1/26/2018.
+ */
+
+public interface OnResponseListener {
+
+
+    void onResponse(String url);
+
+}
